eb2ed62d7508739fa03332fd23064603da8ff14d
Maximilian Rötzler <max.roetzler@gmail.com>
2018-06-11 11:12:34 +0200

    Update DebugShaders.cs
    Added namespace

--- Assets/Plugins/DebugShaders/Editor/DebugShaders.cs
+++ Assets/Plugins/DebugShaders/Editor/DebugShaders.cs
@@ -6,83 +6,86 @@
 using UnityEngine;
 using UnityEditor;
 
-public class DebugShaders
+namespace FantasticYes.Tools
 {
-	private string m_path;
-	private int m_selection;
-	private Shader [] m_shaders;
-	private GUIContent [] m_labels;
-
-	/// <summary>
-	/// Create a new instance of <ctor>DebugShaders</ctor> and initialize it.
-	/// </summary>
-	public DebugShaders ()
+	public class DebugShaders
 	{
-		string [] guids = AssetDatabase.FindAssets ("t:Folder DebugShaders");
+		private string m_path;
+		private int m_selection;
+		private Shader [] m_shaders;
+		private GUIContent [] m_labels;
 
-		if (guids.Length == 1)
+		/// <summary>
+		/// Create a new instance of <ctor>DebugShaders</ctor> and initialize it.
+		/// </summary>
+		public DebugShaders ()
 		{
-			m_path = AssetDatabase.GUIDToAssetPath (guids [0]);
-			guids = AssetDatabase.FindAssets ("t:Shader", new string [1] { m_path });
+			string [] guids = AssetDatabase.FindAssets ("t:Folder DebugShaders");
 
-			int length = guids.Length + 1;
-
-			m_shaders = new Shader [length];
-			m_shaders [0] = null;
-
-			m_labels = new GUIContent [length];
-			m_labels [0] = new GUIContent ("Default");
-
-			for (int i = 1; i < length; i++)
+			if (guids.Length == 1)
 			{
-				m_shaders [i] = AssetDatabase.LoadAssetAtPath<Shader> (AssetDatabase.GUIDToAssetPath (guids [i - 1]));
-				m_labels [i] = new GUIContent (m_shaders [i].name.Substring (6)); // Remove Debug from name
+				m_path = AssetDatabase.GUIDToAssetPath (guids [0]);
+				guids = AssetDatabase.FindAssets ("t:Shader", new string [1] { m_path });
+
+				int length = guids.Length + 1;
+
+				m_shaders = new Shader [length];
+				m_shaders [0] = null;
+
+				m_labels = new GUIContent [length];
+				m_labels [0] = new GUIContent ("Default");
+
+				for (int i = 1; i < length; i++)
+				{
+					m_shaders [i] = AssetDatabase.LoadAssetAtPath<Shader> (AssetDatabase.GUIDToAssetPath (guids [i - 1]));
+					m_labels [i] = new GUIContent (m_shaders [i].name.Substring (6)); // Remove Debug from name
+				}
+			}
+			else
+			{
+				m_shaders = new Shader [1] { null };
+				m_labels = new GUIContent [1] { new GUIContent ("Default") };
+
+				Debug.LogError ("[DebugShaders] Failed to load debug shaders! Make sure the directory: DebugShaders and sub-folder: Shaders exist!");
 			}
 		}
-		else
+
+		/// <summary>
+		/// Draw the DebugShaders UI control.
+		/// </summary>
+		/// <param name="rect"></param>
+		public void Draw (Rect rect)
 		{
-			m_shaders = new Shader [1] { null };
-			m_labels = new GUIContent [1] { new GUIContent ("Default") };
+			EditorGUI.BeginChangeCheck ();
+			m_selection = EditorGUI.Popup (rect, GUIContent.none, m_selection, m_labels);
 
-			Debug.LogError ("[DebugShaders] Failed to load debug shaders! Make sure the directory: DebugShaders and sub-folder: Shaders exist!");
-		}
-	}
-
-	/// <summary>
-	/// Draw the DebugShaders UI control.
-	/// </summary>
-	/// <param name="rect"></param>
-	public void Draw (Rect rect)
-	{
-		EditorGUI.BeginChangeCheck ();
-		m_selection = EditorGUI.Popup (rect, GUIContent.none, m_selection, m_labels);
-
-		if (EditorGUI.EndChangeCheck ())
-		{
-			Shader shader = m_shaders [m_selection];
-
-			// Load additional resources
-			if (m_selection > 0)
+			if (EditorGUI.EndChangeCheck ())
 			{
-				int propertyCount = ShaderUtil.GetPropertyCount (shader);
+				Shader shader = m_shaders [m_selection];
 
-				for (int i = 0; i < propertyCount; i++)
+				// Load additional resources
+				if (m_selection > 0)
 				{
-					if (ShaderUtil.GetPropertyType (shader, i) == ShaderUtil.ShaderPropertyType.TexEnv)
-					{
-						string textureName = ShaderUtil.GetPropertyName (shader, i);
-						Texture2D textureFile = AssetDatabase.LoadAssetAtPath<Texture2D> (m_path + "/Editor/Textures/" + textureName.TrimStart ('_') + ".png");
+					int propertyCount = ShaderUtil.GetPropertyCount (shader);
 
-						if (textureFile != null)
+					for (int i = 0; i < propertyCount; i++)
+					{
+						if (ShaderUtil.GetPropertyType (shader, i) == ShaderUtil.ShaderPropertyType.TexEnv)
 						{
-							Shader.SetGlobalTexture (textureName, textureFile);
+							string textureName = ShaderUtil.GetPropertyName (shader, i);
+							Texture2D textureFile = AssetDatabase.LoadAssetAtPath<Texture2D> (m_path + "/Editor/Textures/" + textureName.TrimStart ('_') + ".png");
+
+							if (textureFile != null)
+							{
+								Shader.SetGlobalTexture (textureName, textureFile);
+							}
 						}
 					}
 				}
-			}
 
-			SceneView.lastActiveSceneView.SetSceneViewShaderReplace (shader, null);
-			SceneView.RepaintAll ();
+				SceneView.lastActiveSceneView.SetSceneViewShaderReplace (shader, null);
+				SceneView.RepaintAll ();
+			}
 		}
 	}
 }
